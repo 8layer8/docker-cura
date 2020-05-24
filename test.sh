4b8b3ba52320648513d98a7e4e82b79ac23932a8
8layer8 <brad@ntbrad.com>
2020-05-24 00:27:30 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,11 @@
-# Dockerfile
-# mkdir -p /root/docker/dockerfiles/dev_workstation
-# git clone git@github.disney.com:wdprt-middleware/wdprt_sece_dev_workstation.git
-# vi /root/docker/dockerfiles/dev_workstation/Dockerfile
-# paste in this file
 # run it with:
 
 # source proxy.sh
-# docker build -t dev_workstation /root/docker/dockerfiles/dev_workstation/
-# docker images
-# docker run -d -p 3389:3389 dev_workstation
-
-
 
 docker rm cura-docker-test
 docker rmi docker-cura
 docker build -t mindcrime30/docker-cura:0.0.1 .
+
 # docker images
 docker run --rm -p 5805:5800 --name docker-cura-test mindcrime30/docker-cura:0.0.1
 
